f2e1702f7f977bc4b3ee353afeeb1a47b228de77
flityzz <jvlds.joao@gmail.com>
2023-10-30 18:30:32 -0300

    updating language json

--- src/i18n/locales/en-us.js
+++ src/i18n/locales/en-us.js
@@ -1,7 +1,34 @@
 const translateObject = {
   translations: {
     home: {
-      message: 'Hello World!'
+      title: 'Brazilian Software House',
+      subTitle: 'The best professionals in the market!'
+    },
+    article: {
+      title: 'Article',
+      text: 'See how BSH professionals accelerated processes for various companies in the country!'
+    },
+    joinUs: {
+      title: 'Join Us!',
+      text: 'Our vocation as a company is always to provide the best customer service experience. We aim to address our customers\' needs comprehensively and in a humanized way, through products that make life easier and connect people.',
+      getStarted: 'Apply Now'
+    },
+    opinions: {
+      title: 'What Our Customers Say',
+      text: 'Excellent experience, BSH has the best team in Brazil.'
+    },
+    contactUs: {
+      title: 'Contact',
+      text: 'We are looking forward to working with you.',
+      name: 'Name',
+      namePlaceholder: 'Your Name',
+      email: 'Email',
+      emailPlaceholder: 'Your Email',
+      message: 'Message',
+      messagePlaceholder: 'Your Message',
+      buttonSendMessage: 'Send',
+      address: '123 Main Street, Anytown USA',
+      phone: 'Phone'
     }
   }
 }
--- src/i18n/locales/pt-br.js
+++ src/i18n/locales/pt-br.js
@@ -1,7 +1,34 @@
 const translateObject = {
   translations: {
     home: {
-      message: 'Ola Mundo!'
+      title: 'Software House Brasil',
+      subTitle: 'Os melhores profissionais do mercado!'
+    },
+    article:{
+      title: 'Artigo',
+      text: 'Veja como os profissionais da SHB aceleraram os processos de diversas empresas no país!'
+    },
+    joinUs:{
+      title: 'Junte-se a nós!',
+      text: 'Nossa vocação como empresa é proporcionar sempre a melhor experiência de atendimento. Buscamos solucionar as necessidades dos nossos clientes de forma ampla e humanizada, por meio de produtos que facilitem a vida e conectem as pessoas.',
+      getStarted: 'Candidate-se'
+    },
+    opinions:{
+      title: 'O que nossos clientes dizem',
+      text: 'Experiência excelente, a SHB tem a melhor equipe do brasil'
+    },
+    contactUs:{
+      title: 'Contato',
+      text: 'Estamos ansiosos para trabalhar com você',
+      name: 'Nome',
+      namePlaceholder: 'Seu Nome',
+      email: 'Email',
+      emailPlaceholder: 'Seu Email',
+      message: 'Mensagem',
+      mesagePlaceholder: 'Sua Mensagem',
+      buttonSendMessage: 'Enviar',
+      address:'123 Main Street, Anytown USA',
+      phone: 'Telefone'
     }
   }
 }

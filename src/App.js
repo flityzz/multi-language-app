9ce2390d6a6d34418edc620d712d221ff5180d90
flityzz <jvlds.joao@gmail.com>
2023-10-30 14:51:41 -0300

    adding the  Flag component to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import { useTranslation } from 'react-i18next'
 import { BrasilFlag, EuaFlag } from './assets'
 
-import Flag from './Flag'
+import Flag from './components/Flag'
 
 function App() {
+
+  const { i18n } = useTranslation()
+  const selectedLanguage = i18n.language
+
+  function handleChangeLanguage(language) {
+    i18n.changeLanguage(language)
+    console.log(language)
+  }
+  
   return (
     <div className="App">
-      <h1>test</h1>
+      <div className="flags-container">
+        <Flag
+          image={BrasilFlag}
+          isSelected={selectedLanguage === 'pt-BR'} 
+          onClick={() => handleChangeLanguage('pt-BR')} 
+        />
+        
+        <Flag
+          image={EuaFlag}
+          isSelected={selectedLanguage === 'en-US'} 
+          onClick={() => handleChangeLanguage('en-US')} 
+        />
+      </div>
+      
+      <div>
+        <h1>test</h1>
+      </div>
     </div>
   );
 }

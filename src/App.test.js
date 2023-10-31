97c97922f087dcc83c1e89a60ba2effb90ed3f4f
flityzz <jvlds.joao@gmail.com>
2023-10-30 21:01:25 -0300

    adding switch language test

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,44 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from "react-router-dom";
-import { useTranslation } from 'react-i18next'
-
+import {cleanup, render, screen, fireEvent} from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import '../src/i18n'
+
+afterEach(cleanup);
 
 test('renders the component with language flags', () => {
 
-  render(<Router>
-    <App />
-  </Router>);
-  const { i18n } = useTranslation();
+  render(
+    <Router>
+      <App />
+    </Router>,
+    );
+
 
   const brFlag = screen.getByAltText('Brazil Flag');
   const usFlag = screen.getByAltText('USA Flag');
 
   expect(brFlag).toBeInTheDocument();
   expect(usFlag).toBeInTheDocument();
+});
+
+test('switch the language of the app', () => {
+
+  render(
+    <Router>
+      <App />
+    </Router>,
+    );
+
+
+  const usFlag = screen.getByAltText('USA Flag');
+
+  expect(usFlag).toBeInTheDocument();
+
+  fireEvent.click(usFlag);
+
+  
+  const someElementWithEnglishText = screen.getByText('Brazilian Software House'); 
+
+  expect(someElementWithEnglishText).toBeInTheDocument();
 });
 

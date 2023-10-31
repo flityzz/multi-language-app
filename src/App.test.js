2c704d01e0fce6256fadca2a3a53d7c0369a64ac
flityzz <jvlds.joao@gmail.com>
2023-10-30 22:14:39 -0300

    fixing imports

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import {cleanup, render, screen, fireEvent} from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from '../App';
-import '../i18n'
+import App from '../src/App';
+import '../src/i18n'
 
 afterEach(cleanup);
 
